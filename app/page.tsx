14dfffdc8f5682744dd2653bfd7770905b50629d
lokesh <lokeshrajeshbabu28@gmail.com>
2024-11-25 22:23:34 +0530

    First commit

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,7 @@ export default function Home() {
           height={38}
           priority
         />
+        <h1>welcome to next js</h1>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
@@ -21,10 +22,11 @@ export default function Home() {
             .
           </li>
           <li>Save and see your changes instantly.</li>
+
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
+          {/* <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
@@ -38,19 +40,19 @@ export default function Home() {
               height={20}
             />
             Deploy now
-          </a>
-          <a
+          </a> */}
+          {/* <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
           >
             Read our docs
-          </a>
+          </a> */}
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
+        {/* <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           target="_blank"
@@ -64,8 +66,8 @@ export default function Home() {
             height={16}
           />
           Learn
-        </a>
-        <a
+        </a> */}
+        {/* <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           target="_blank"
@@ -79,14 +81,14 @@ export default function Home() {
             height={16}
           />
           Examples
-        </a>
-        <a
+        </a> */}
+        {/* <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          <Image
+        > */}
+          {/* <Image
             aria-hidden
             src="/globe.svg"
             alt="Globe icon"
@@ -94,7 +96,7 @@ export default function Home() {
             height={16}
           />
           Go to nextjs.org →
-        </a>
+        </a> */}
       </footer>
     </div>
   );
